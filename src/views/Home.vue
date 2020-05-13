97fa112cd57f39186b186061e9d99688b7b7ad70
xiaolong666666 <long.1585097369@qq.com>
2020-05-13 11:51:06 +0800

    swiper组件完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
   import HomeHeader from './../components/Home/Header'
+  import HomeSwiper from './../components/Home/Swiper'
   export default {
     name: 'Home',
-    components: { HomeHeader }
+    components: { HomeHeader, HomeSwiper }
   }
 </script>
 
